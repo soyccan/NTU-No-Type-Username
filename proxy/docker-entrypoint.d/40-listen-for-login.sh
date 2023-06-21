65cee74f2cc45f9065f83f2ed46c0367632e4918
soyccan <soyccan@gmail.com>
2023-06-21 12:29:14 +0800

    Improve Reliability

--- proxy/docker-entrypoint.d/40-listen-for-login.sh
+++ proxy/docker-entrypoint.d/40-listen-for-login.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-if [ -S "$SOCK_PATH" ]; then
-    rm "$SOCK_PATH"
-fi
-
 # reload NGINX configs after a periodic login is done
 # (signaled by a unix socket connection),
 # so that latest cookies are used
-socat -d -d -lf /dev/stderr UNIX-LISTEN:"$SOCK_PATH" EXEC:'nginx -s reload' &
+(while true; do
+    if [ -S "$SOCK_PATH" ]; then
+        rm "$SOCK_PATH"
+    fi
+    socat -d -d -lf /dev/stderr UNIX-LISTEN:"$SOCK_PATH" EXEC:'nginx -s reload'
+done &)
